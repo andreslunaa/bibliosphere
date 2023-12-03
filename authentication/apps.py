cb1e2d03218db9083cba6350f4a2ca4f29bda8fb
REYNOLDS DUDDU <61578612+reynoldsduddu@users.noreply.github.com>
2023-12-03 12:42:39 -0500

    Update apps.py

--- authentication/apps.py
+++ authentication/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-
+# Configuration class for the 'authentication' app
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
